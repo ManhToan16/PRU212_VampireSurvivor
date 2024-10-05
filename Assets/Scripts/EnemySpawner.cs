3276a652481562ed8b69ab30422f4f2cd81e1a74
ManhToan16 <toanluon507@gmail.com>
2024-10-05 22:37:18 +0700

    spawn outside the screen

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,6 +8,7 @@ public class EnemySpawner : MonoBehaviour
     public GameObject enemyToSpawn;
     public float timeToSpawn;
     private float spawnCounter;
+    public Transform minPoint, maxPoint;
     void Start()
     {
         spawnCounter=timeToSpawn;
@@ -19,7 +20,40 @@ public class EnemySpawner : MonoBehaviour
         spawnCounter-=Time.deltaTime;
         if (spawnCounter <= 0) {
             spawnCounter = timeToSpawn;
-            Instantiate(enemyToSpawn,transform.position,transform.rotation);
+            //Instantiate(enemyToSpawn,transform.position,transform.rotation);
+            Instantiate(enemyToSpawn, SelectSpawnPoint(),transform.rotation);
         }
+    }
+    public Vector3 SelectSpawnPoint()
+    {
+        Vector3 spawnPoint= Vector3.zero;
+        bool spawnVerticalEdge = Random.Range(0f, 1f) > 0.5f;
+        if (spawnVerticalEdge)
+        {
+            spawnPoint.y=Random.Range(minPoint.position.y, maxPoint.position.y);
+            if (Random.Range(0f, 1f) > .5f)
+            {
+                spawnPoint.x = maxPoint.position.x;
+
+            }
+            else
+            {
+                spawnPoint.x=minPoint.position.x;   
+            }
+        }
+        else
+        {
+            spawnPoint.x = Random.Range(minPoint.position.x, maxPoint.position.x);
+            if (Random.Range(0f, 1f) > .5f)
+            {
+                spawnPoint.y = maxPoint.position.y;
+
+            }
+            else
+            {
+                spawnPoint.y = minPoint.position.y;
+            }
+        }
+        return spawnPoint;
     }
 }
